defd8a1186d6e1a8d2c51d16c9b55d66355912b7
Dmitriifd <idimka9@gmail.com>
2023-09-21 14:06:43 +0500

    server components

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import NavBar from '@/components/NavBar';
 import { Separator } from '@/components/ui/separator';
 import { ThemeProvider } from '@/providers/ThemeProvider';
 import { cn } from '@/lib/utils';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -41,7 +42,10 @@ export default function RootLayout({
             >
               <NavBar />
               <Separator />
-              {children}
+              <main className='flex flex-grow w-full justify-center items-center dark:bg-neutral-950'>
+                {children}
+                <Toaster />
+              </main>
             </div>
           </body>
         </ThemeProvider>
